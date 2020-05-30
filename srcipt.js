02f02de5115eea74a065090551fcaa7128bb5fd9
Nartynka <mplutwska@gmail.com>
2020-05-30 20:05:16 +0200

    working, but without - between letters

--- srcipt.js
+++ srcipt.js
@@ -1,6 +1,6 @@
 let password = "gofer"; //password
 password = password.toUpperCase();
-var password2 = ""; //dashed password
+let password2 = ""; //dashed password
 const letter = document.getElementsByClassName("letters_item");
 const alfabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ";
 const container = document.querySelector(".letters");
@@ -27,27 +27,48 @@ function divs() {
     letter.innerHTML = alfabet.charAt(i);
     container.appendChild(letter);
   }
-  for (let i=0; i < 35; i++) {
-    letter[i].addEventListener('click', sprawdz, false);
-  }
+  // for (let i=0; i < 35; i++) {
+  //   letter[i].addEventListener('click', sprawdz, false);
+  // }
+  const letters = document.querySelectorAll(".letters_item");
+  letters.forEach(letters => letters.addEventListener('click', sprawdz, false));
 }
-
-
+let count = 0;
+let countn = 0;
+let tab = [...password2];
 function sprawdz(){
+  // const tab2 = [...password];
+  //console.log(tab);
   for (let i = 0; i < password.length; i++) {
     if (password.charAt(i) == this.innerHTML) {
-    password2 = password2.replace(password2.charAt(i), this.innerHTML);
-    console.log(password2);
-    this.classList.add("correct");
-    this.removeEventListener('click', sprawdz);
-    document.querySelector(".password").innerHTML = password2;
-      if(password2==password){
-        console.log("WYGRAŁEŚ, BRAWO!");
-      }
-  } 
-  else if(password.charAt(i) != this.innerHTML){
+      count++;
+      tab.splice(i, 0, this.innerHTML);
+      this.classList.add("correct");
+      this.removeEventListener('click', sprawdz);
+      document.querySelector(".password").innerHTML = tab.join('');
+      console.log(tab);
+      if(count == password.length) end(1);
+    } 
+  }
+  if(!this.classList.contains("correct")) {
     this.classList.add("incorrect");
     this.removeEventListener('click', sprawdz);
+    countn++;
+    if(countn == 9) end(0);
   }
+  console.log("incorrect: " + countn);
+  console.log("correct: "+count);
 }
+const l = document.querySelector(".end");
+const lp = document.querySelector(".end_p");
+function end(wl){
+  l.setAttribute("display", "block");
+  if(wl == 1){
+    console.log("WYGRAŁEŚ, BRAWO!");
+    lp.innerHTML = "Wygrałeś, gratki!";
+  }
+  else {
+    console.log("Przegrałeś, Spróbuj ponownie");
+    lp.innerHTML = "Przykro mi, przegrałeś";
+  }
 }
